8fb2297fc4be779ac803e088c6dde039b3aad40c
Mehmet Salih Aslan <aslanmsalih@gmail.com>
2022-01-12 20:43:06 +0300

    Initial Commit

---
+++ SpaceX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceX
+//
+//  Created by Mehmet Salih Aslan on 12.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Mehmet Salih Aslan on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
